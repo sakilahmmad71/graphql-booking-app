f58c64868d6f1adbebddab3ce62adb070fd83364
sakilahmmad71 <sakilahmmad71@gmail.com>
2020-09-01 12:27:16 +0600

    functionality updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import './App.css'
 
-function App() {
+import Auth from './pages/Auth'
+import Events from './pages/Events'
+import Bookings from './pages/Bookings'
+import Header from './components/layout/Header'
+
+import authContext from './context/authContext'
+
+const App = () => {
     return (
-        <div className="App">
-            <h1>React started for front-end</h1>
-        </div>
+        <BrowserRouter>
+            <>
+                <authContext.Provider>
+                    <Header />
+                    <main className="main">
+                        <Switch>
+                            <Redirect from="/" to="/auth" exact />
+                            <Route path="/auth">
+                                <Auth />
+                            </Route>
+                            <Route path="/events">
+                                <Events />
+                            </Route>
+                            <Route path="/bookings">
+                                <Bookings />
+                            </Route>
+                        </Switch>
+                    </main>
+                </authContext.Provider>
+            </>
+        </BrowserRouter>
     )
 }
 
