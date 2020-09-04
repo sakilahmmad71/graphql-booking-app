cf298d5c038b8591a4e2a5fa7963a9d47588ce8a
sakilahmmad71 <sakilahmmad71@gmail.com>
2020-09-04 21:36:53 +0600

    Spinner added and indicating through where does it needed

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
 import './Header.css'
+import { authContext } from '../../App'
+import { logout } from '../../context/authReducer.js'
 
 const Header = () => {
+    const { authState, authDispatch } = useContext(authContext)
+
+    const handleLogout = () => {
+        authDispatch(logout())
+    }
+
     return (
         <header className="nav">
             <div className="nav__logo">
@@ -10,15 +18,27 @@ const Header = () => {
             </div>
             <nav className="nav__item">
                 <ul>
-                    <li>
-                        <NavLink to="/auth">Authenticate</NavLink>
-                    </li>
+                    {!authState.token && (
+                        <li>
+                            <NavLink to="/auth">Authenticate</NavLink>
+                        </li>
+                    )}
+
                     <li>
                         <NavLink to="/events">Events</NavLink>
                     </li>
-                    <li>
-                        <NavLink to="/bookings">Bookings</NavLink>
-                    </li>
+
+                    {authState.token && (
+                        <li>
+                            <NavLink to="/bookings">Bookings</NavLink>
+                        </li>
+                    )}
+
+                    {authState.token && (
+                        <li>
+                            <button onClick={handleLogout}>Logout</button>
+                        </li>
+                    )}
                 </ul>
             </nav>
         </header>
